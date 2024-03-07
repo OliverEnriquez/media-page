f55895b1b9ce39015b08fb3d58a3d1d178c92ab9
Oliver Enriquez <oenriquez@1aauto.com>
2024-03-06 19:15:18 -0600

    Adding events

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,18 +1,37 @@
 import React, { useEffect, useState } from 'react';
 
 const Content: React.FC = () => {
-    const [colorSelected, setColorSelected] = useState<string>('"text-blue-700 hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:bg-gray-900 dark:focus:ring-blue-800');
+    const [selectedCategory, setSelectedCategory] = useState<string>("All categories");
 
-
+    const handleCategoryChange = (category: string) => {
+        alert(category);
+        setSelectedCategory(category);
+    };
 
     return (
         <div className="container mx-auto px-4">
-
-
             <div className="flex items-center justify-center py-4 md:py-8 flex-wrap">
-                <button type="button" className={colorSelected}>All categories</button>
-                <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800">Movies</button>
-                <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800">Gaming</button>
+                <button type="button"
+                    className={selectedCategory === "All categories"
+                        ? "text-blue-700 hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:bg-gray-900 dark:focus:ring-blue-800"
+                        : "text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800"}
+                    onClick={() => handleCategoryChange("All categories")}>
+                    All categories
+                </button>
+                <button type="button"
+                    className={selectedCategory === "Movies"
+                        ? "text-blue-700 hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:bg-gray-900 dark:focus:ring-blue-800"
+                        : "text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800"}
+                    onClick={() => handleCategoryChange("Movies")}>
+                    Movies
+                </button>
+                <button type="button"
+                    className={selectedCategory === "Gaming"
+                        ? "text-blue-700 hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:bg-gray-900 dark:focus:ring-blue-800"
+                        : "text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800"}
+                    onClick={() => handleCategoryChange("Gaming")}>
+                    Gaming
+                </button>
             </div>
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
                 <div>
@@ -21,13 +40,11 @@ const Content: React.FC = () => {
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
                         <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
                     </a>
-
                 </div>
-
             </div>
+        </div>
+    );
 
-        </div >
-    )
 }
 
 export default Content;
